391c4878df763b432579b4d2fe5ca0f1f29c30e9
Juan8516 <juan.ind85@gmail.com>
2025-01-07 10:14:44 -0500

    feat: se crean preguntas para registro y login al usuario

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ print("""
 accion = input("Que quieres hacer ?: ")
 
 if accion == "registro":
-    print("Ok, vamos a registrarte !!")
+    print("\nOk, vamos a registrarte !!")
+    nombre    = input("Ingresa tu nombre: ")
+    apellidos = input("Ingresa tus apellidos: ")
+    email     = input("Ingresa tu email: ")
+    password  = input("Ingresa tu contraseña: ")
+    
 elif accion == "login":
     print("Ingresa usuario y contraseña: ")
+    email     = input("Ingresa tu email: ")
+    password  = input("Ingresa tu contraseña: ")
