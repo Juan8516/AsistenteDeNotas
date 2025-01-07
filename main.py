60e5d7646c714ea8e042706ace18e993c2bb7fe9
Juan8516 <juan.ind85@gmail.com>
2025-01-06 23:05:06 -0500

    feat: preguntas para el usuario

---
+++ main.py
@@ -0,0 +1,12 @@
+#Preguntas del asistente
+print("""
+      - registro
+      - login
+      """)
+
+accion = input("Que quieres hacer ?: ")
+
+if accion == "registro":
+    print("Ok, vamos a registrarte !!")
+elif accion == "login":
+    print("Ingresa usuario y contraseña: ")
